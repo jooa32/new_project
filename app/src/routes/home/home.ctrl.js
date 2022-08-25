2213a470ff58c483c290aecfa46b651a0229bac0
jooa <dlwndk4677@gmail.com>
2022-08-25 12:16:03 +0900

    login

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     hello: (req, res) => {
@@ -16,24 +16,13 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id, 
-         psword = req.body.psword;
-
-        // const userStorage = new UserStorage();
-        const users = UserStorage.getUsers("id", "psword");
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false; 
-        response.msg = "로그인에 실패하였습니다.";
+        // 클라이언트가 전달한 req
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
-    },
+        // 클라이언트에 res 로 던져줌 
+        // return res.json(response);
+    }
 };
 
 
@@ -41,3 +30,5 @@ module.exports = {
     output,
     process,
 }
+
+
